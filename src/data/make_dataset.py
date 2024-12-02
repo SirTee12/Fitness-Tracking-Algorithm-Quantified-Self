bbfee25004848e5728ed5201975f7f279af9489d
SirTee12 <ahmadayantola@gmail.com>
2024-12-02 22:37:25 +0100

    created a new ipynb file replica of the original .py file

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,17 +6,23 @@ from glob import glob
 # --------------------------------------------------------------
 single_file_acc = pd.read_csv(
     "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv")
-
+single_file_gyr = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv")
 
 # --------------------------------------------------------------
 # List all data in data/raw/MetaMotion
 # --------------------------------------------------------------
-
-
+files = glob("../../data/raw/MetaMotion/*.csv")
+len(files)
 # --------------------------------------------------------------
 # Extract features from filename
 # --------------------------------------------------------------
+data_path = "../../data/raw/MetaMotion\\"
+f = files[0]
 
+participant = f.split('-')[0].replace(data_path, '')
+label = f.split('-')[1]
+category = f.split('-')[2].rstrip('123')
 
 # --------------------------------------------------------------
 # Read all files
@@ -36,7 +42,6 @@ single_file_acc = pd.read_csv(
 # --------------------------------------------------------------
 # Merging datasets
 # --------------------------------------------------------------
-
 
 # --------------------------------------------------------------
 # Resample data (frequency conversion)
